7f6aa23ea0f86ac86584b0732350f18eaf5cede5
Alejandro González Reyes <alexglzrys@gmail.com>
2020-01-11 15:31:37 -0600

    Retrieve and return all tasks registered in the database

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -13,7 +14,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::all();
+        return $tasks;
     }
 
     /**
