9a2112e2e6eceed1f3c91463c2182893b783679d
Alejandro González Reyes <alexglzrys@gmail.com>
2020-01-11 15:17:36 -0600

    create static view for the list of all tasks

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,5 +2,34 @@
 @extends('app')
 
 @section('content')
-  <h1>Dashboard aplicación Vue con Laravel</h1>
+  <div id="vue-crud" class="row">
+    <div class="col-sm-12">
+      <div class="jumbotron">
+        <h1 class="display-4">CRUD Laravel y Vuejs</h1>
+        <p class="lead">Conexión a base de datos y solicitud de servicios a través de Axios</p>
+      </div>
+    </div>
+    <div class="col-sm-7">
+      <table class="table table-hover table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Tarea</th>
+            <th colspan="2">&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td width="10px">1</td>
+            <td>Correr 10km</td>
+            <td width="10px"><a href="#" class="btn btn-warning btn-sm">Editar</a></td>
+            <td width="10px"><a href="#" class="btn btn-danger btn-sm">Eliminar</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="col-sm-5">
+      <pre></pre>
+    </div>
+  </div>
 @endsection
